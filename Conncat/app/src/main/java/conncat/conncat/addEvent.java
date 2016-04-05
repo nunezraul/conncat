ee85a40b861fbd581e9d3d6b395fb2565f6ea8d4
michelle <michellezhou@yahoo.com>
2016-04-04 18:35:27 -0700

    Comments added

--- Conncat/app/src/main/java/conncat/conncat/addEvent.java
+++ Conncat/app/src/main/java/conncat/conncat/addEvent.java
@@ -8,6 +8,12 @@ import android.support.v7.widget.Toolbar;
 public class addEvent extends AppCompatActivity{
     private Toolbar toolbar;
 
+    /*
+    This function sets up the toolbar
+
+    @param  savedInstanceState  any saved information related to current activity state
+    @see    toolbar
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
