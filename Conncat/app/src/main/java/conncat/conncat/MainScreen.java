8c47d8052686a7648a6022b64b7749d015313f89
12jchuang <12jchuang@gmail.com>
2016-04-13 18:21:56 -0700

    small changes to side navigation bar, more XML cleanup

--- Conncat/app/src/main/java/conncat/conncat/MainScreen.java
+++ Conncat/app/src/main/java/conncat/conncat/MainScreen.java
@@ -129,17 +129,17 @@ public class MainScreen extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_categories) {
 
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_notif) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_sett) {
 
         }
 
